7666ed6ce06b78d09a027db50fed56f2f0919760
Moon Natalia <nataliya.moon@gmail.com>
2025-09-11 23:32:46 +0500

    style: add homePage styles

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,9 +1,46 @@
-import type { ReactNode } from 'react';
+'use client';
+
+import { RoutePath } from '@/common/constants';
+import { useAuth } from '@/components/ProvidersWrapper/AuthProvider/AuthContext';
+import { Link } from '@i18n/navigation';
+import { type ReactNode } from 'react';
+import styles from './HomePage.module.scss';
 
 export default function HomePage(): ReactNode {
+  const { isAuth, currentUser } = useAuth();
+
   return (
-    <main>
-      <h1>Home Page</h1>
-    </main>
+    <section className={styles.main}>
+      {!isAuth ? (
+        <div className={styles.guest}>
+          <h1 className={styles.heading}>Welcome!</h1>
+          <div className={styles.actions}>
+            <Link href={RoutePath.Signin} className={styles.link}>
+              Sign In
+            </Link>
+            <Link href={RoutePath.Signup} className={styles.link}>
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.user}>
+          <h1 className={styles.heading}>
+            Welcome Back, {currentUser?.displayName ?? currentUser?.email}!
+          </h1>
+          <nav className={styles.nav}>
+            <Link href={RoutePath.Home} className={styles.link}>
+              REST Client
+            </Link>
+            <Link href={RoutePath.History} className={styles.link}>
+              History
+            </Link>
+            <Link href={RoutePath.Variables} className={styles.link}>
+              Variables
+            </Link>
+          </nav>
+        </div>
+      )}
+    </section>
   );
 }
