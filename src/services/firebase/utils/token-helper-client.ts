c0148f3174d360a19fdbebd2c17b66a9439d146c
dusixx <113414935+dusixx@users.noreply.github.com>
2025-09-15 02:39:41 +0300

    refactor: update auth logic

--- src/services/firebase/utils/token-helper-client.ts
+++ src/services/firebase/utils/token-helper-client.ts
@@ -1,26 +1,25 @@
-const USER_ID_NAME = 'fb-idToken-Qf0Hr3Y9';
-const DEFAULT_MAX_AGE = 60 * 24 * 5;
+export const USER_ID_TOKEN_NAME = 'fb-idToken-Qf0Hr3Y9';
+const DEFAULT_MAX_AGE = 3600 * 24 * 5;
 const DEFAULT_PATH = '/';
 
 type TokenCookieOptions = Partial<{
   httpOnly: boolean;
   secure: boolean;
-  maxAgeMinutes: number;
+  maxAgeSeconds: number;
   path: string;
 }>;
 
 const setToken = (token: string, options?: TokenCookieOptions): void => {
   const {
-    maxAgeMinutes = DEFAULT_MAX_AGE,
+    maxAgeSeconds = DEFAULT_MAX_AGE,
     path = DEFAULT_PATH,
     httpOnly = false,
     secure = true,
   } = options ?? {};
 
-  const maxAgeSeconds = 60 * maxAgeMinutes;
   const params = [
-    `${USER_ID_NAME}=${encodeURIComponent(token)}`,
-    `max-age=${maxAgeSeconds.toString()}`,
+    `${USER_ID_TOKEN_NAME}=${encodeURIComponent(token)}`,
+    `max-age=${Math.round(maxAgeSeconds).toString()}`,
     `path=${path}`,
     `samesite=lax`,
     httpOnly ? 'httpOnly' : '',
@@ -30,18 +29,22 @@ const setToken = (token: string, options?: TokenCookieOptions): void => {
 };
 
 const getToken = (): string => {
-  const token = document.cookie.match(RegExp(`${USER_ID_NAME}=([^;]+)`))?.[1] ?? '';
+  const token = document.cookie.match(RegExp(`${USER_ID_TOKEN_NAME}=([^;]+)`))?.[1] ?? '';
   return decodeURIComponent(token);
 };
 
 const removeToken = (): void => {
-  if (RegExp(`${USER_ID_NAME}=`).test(document.cookie)) {
-    document.cookie = `${USER_ID_NAME}=;expires=${new Date(0).toUTCString()};path=/;`;
+  if (RegExp(`${USER_ID_TOKEN_NAME}=`).test(document.cookie)) {
+    document.cookie = [
+      `${USER_ID_TOKEN_NAME}=`,
+      `expires=${new Date(0).toUTCString()}`,
+      `max-age=-1`,
+    ].join(';');
   }
 };
 
 export const TokenCookieHelper = {
-  name: USER_ID_NAME,
+  name: USER_ID_TOKEN_NAME,
   set: setToken,
   get: getToken,
   remove: removeToken,
