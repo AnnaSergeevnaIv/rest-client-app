2f7bf258b9cdf8630e92cc08b638d2e76280e0b9
Moon Natalia <nataliya.moon@gmail.com>
2025-09-16 13:20:11 +0500

    feat: add AboutUsPage.test and PersonCard.test

--- src/data/team-data.ts
+++ src/data/team-data.ts
@@ -4,7 +4,6 @@ export type PersonData = {
   id: number;
   name: string;
   role: string;
-  bio: string;
   brief: string;
   avatar: string;
   github: string;
@@ -15,7 +14,6 @@ export const teamData: PersonData[] = [
     id: 1,
     name: 'Nataliya Moon',
     role: 'Developer',
-    bio: `I based in Almaty. Graduated from school with honors. Hold a red diploma in Journalism and a Master’s degree in Social Sciences. A few years ago made a career shift to IT — completed a Fullstack Developer course (JavaScript + Node.js) and later joined the RSS course to deepen technical skills. Enjoy planning and structuring.`,
     brief: `Hold a red diploma in Journalism and a Master’s degree in Social Sciences. A few years ago made a career shift to fullstack dev. Enjoy planning and structuring.`,
     avatar: '/avatars/natasha.jpg',
     github: 'https://github.com/nataliyamoon',
@@ -24,7 +22,6 @@ export const teamData: PersonData[] = [
     id: 2,
     name: 'Anna Ivanova',
     role: 'Team Lead | Developer',
-    bio: ``,
     brief: `Believes that on the path a wrong turn was taken... On the way to development, accidentally turned off and became an engineer in relay protection and automation. Trying to correct this unfortunate mistake.`,
     avatar: '/avatars/anna.jpg',
     github: 'https://github.com/AnnaSergeevnaIv',
@@ -33,7 +30,6 @@ export const teamData: PersonData[] = [
     id: 3,
     name: 'Andrew M',
     role: 'Developer',
-    bio: '',
     brief: `Passionate about learning new technologies and building innovative projects. Key softskills: responsibility, attention to detail, eagerness to learn.`,
     avatar: '/avatars/andrew.jpg',
     github: 'https://github.com/dusixx',
