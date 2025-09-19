c1aa2ed30c8a0ee09dd5992e32cec00b4e4c521b
dusixx <113414935+dusixx@users.noreply.github.com>
2025-09-20 02:20:28 +0300

    feat: implement firestore admin service

--- src/components/pages/HistoryPage/HistoryPage.tsx
+++ src/components/pages/HistoryPage/HistoryPage.tsx
@@ -1,10 +1,44 @@
-import type { ReactNode } from 'react';
+'use client';
+
+import { Button } from '@/components/UI/Button/Button.tsx';
+import { Input } from '@/components/UI/Input/Input.tsx';
+import '@/services/firebase/admin/request-history/actions';
+import { useRequestHistory } from '@/services/firebase/admin/request-history/useRequestHistory.ts';
+import { historyEntryMock } from '@/services/firebase/admin/request-history/utils.ts';
+import { useState, type ReactNode } from 'react';
 
 export default function SignupPage(): ReactNode {
+  const [value, setValue] = useState('');
+  const { addEntry, deleteEntry, queryEntries } = useRequestHistory();
+
   return (
     <section>
       <h1>Request history</h1>
       <p>Saved queries are stored here.</p>
+      <Button
+        label='Add entry'
+        onClick={() => {
+          void addEntry(historyEntryMock).then(console.debug);
+        }}
+      />
+      <Button
+        label='Get ALL'
+        onClick={() => {
+          void queryEntries().then(console.debug);
+        }}
+      />
+      <Button
+        label='Delete entry'
+        onClick={() => {
+          void deleteEntry(value).then(console.debug);
+        }}
+      />
+      <Input
+        value={value}
+        onChange={e => {
+          setValue(e.target.value);
+        }}
+      />
     </section>
   );
 }
