38866b139864ef2863685e256bbffa75f63ee6c3
Moon Natalia <nataliya.moon@gmail.com>
2025-09-21 01:10:23 +0500

    feat: add some tests

---
+++ src/components/pages/ErrorPage/ErrorPage.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import ErrorPage from './ErrorPage';
+import { RoutePath } from '@/common/constants/index.ts';
+import { vi } from 'vitest';
+
+vi.mock('next/image', () => ({
+  default: (props: { src: string; alt: string; width: number; height: number }) => (
+    <img src={props.src} alt={props.alt} width={props.width} height={props.height} />
+  ),
+}));
+
+vi.mock('@/components/NavButton/NavBtn.tsx', () => ({
+  NavBtn: (props: { href: string; text: string }) => <a href={props.href}>{props.text}</a>,
+}));
+
+describe('ErrorPage', () => {
+  it('renders image, text and NavBtn', () => {
+    render(<ErrorPage />);
+
+    const image = screen.getByAltText('Page not found');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', '/404.jpg');
+
+    const text = screen.getByText('Page not found');
+    expect(text).toBeInTheDocument();
+
+    const link = screen.getByText('Go Home');
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', RoutePath.Home);
+  });
+});
